7be1f4480d371a7e85be29380d52b2d6c2217bb6
PingVideology <jye@videologyinc.com>
2026-01-23 17:09:37 +0000

    AB#1923. Tested 3 sh and decide only crop one works well ;-)

--- thermal_detection/gst_stream_thermal_crop.sh
+++ thermal_detection/gst_stream_thermal_crop.sh
@@ -4,10 +4,9 @@
 
 gst-launch-1.0 \
   v4l2src device=/dev/video0 ! \
-  video/x-raw,format=NV12,width=640,height=512,framerate=9/1 ! \
+  video/x-raw,format=NV12,width=640,height=512,framerate=9/1 ! videocrop top=96 left=160 right=160 bottom=96 ! \
   tee name=t \
   t. ! queue leaky=no max-size-buffers=30 max-size-bytes=0 max-size-time=0 ! \
-  videocrop top=96 left=160 right=160 bottom=96 ! \
   videoconvert ! video/x-raw,width=320,height=320,format=RGB ! \
   tensor_converter ! \
   tensor_transform mode=arithmetic option=typecast:float32,add:0.0,div:255.0 ! \
