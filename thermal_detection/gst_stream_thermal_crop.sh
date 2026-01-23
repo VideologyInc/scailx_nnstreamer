a8a4dd60dd9f5a0836050cfe3f247a6b3ab6816c
PingVideology <jye@videologyinc.com>
2026-01-23 10:10:28 -0500

    AB#1923. Updated Boson gst formats and added 3 bash scripts to do gst videoscale, videocrop (on 640 x 512 format) and videobox (on grayscale 320 x 256 format).

--- thermal_detection/gst_stream_thermal_crop.sh
+++ thermal_detection/gst_stream_thermal_crop.sh
@@ -4,18 +4,19 @@
 
 gst-launch-1.0 \
   v4l2src device=/dev/video0 ! \
-  video/x-raw,format=NV12,width=640,height=512,framerate=15/2 ! \
+  video/x-raw,format=NV12,width=640,height=512,framerate=9/1 ! \
   tee name=t \
   t. ! queue leaky=no max-size-buffers=30 max-size-bytes=0 max-size-time=0 ! \
-  videoscale ! videoconvert ! video/x-raw,width=320,height=320,format=RGB ! \
+  videocrop top=96 left=160 right=160 bottom=96 ! \
+  videoconvert ! video/x-raw,width=320,height=320,format=RGB ! \
   tensor_converter ! \
   tensor_transform mode=arithmetic option=typecast:float32,add:0.0,div:255.0 ! \
   queue leaky=no max-size-buffers=30 max-size-bytes=0 max-size-time=0 ! \
   tensor_filter latency=1 framework=tensorflow2-lite model=thermal_f16.tflite ! \
   tensor_transform mode=transpose option=1:0:2:3 ! \
   queue leaky=no max-size-buffers=30 max-size-bytes=0 max-size-time=0 ! \
-  tensor_decoder mode=bounding_boxes option1=yolov8 option2=labels.txt option4=640:512 option5=320:320 ! \
-  videoscale ! videoconvert ! video/x-raw,width=640,height=512 ! \
+  tensor_decoder mode=bounding_boxes option1=yolov8 option2=labels.txt option4=320:320 option5=320:320 ! \
+  videoconvert ! video/x-raw,width=320,height=320 ! \
   mix.sink_0 \
   t. ! queue leaky=no max-size-buffers=30 max-size-bytes=0 max-size-time=0 ! \
   videoconvert ! mix.sink_1 \
