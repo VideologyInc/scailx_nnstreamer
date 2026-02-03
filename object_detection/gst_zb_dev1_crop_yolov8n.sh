80738e43c6b2d4eb1fd92f95c9808d69befaf396
PingVideology <jye@videologyinc.com>
2026-02-03 20:54:02 +0000

    AB#1954. Copied and renamed object dteection bash scripts to 2 x ZB and 2 x Boson. The 2 ZB scripts work well.

---
+++ object_detection/gst_zb_dev1_crop_yolov8n.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+gst-launch-1.0 \
+  v4l2src device=/dev/video1 ! video/x-raw,width=640,height=360 ! \
+  videorate max-rate=10 ! video/x-raw,framerate=10/1 ! \
+  queue leaky=2 max-size-buffers=10 ! videocrop top=20 left=160 right=160 bottom=20 ! \
+  tee name=t \
+  t. ! queue leaky=2 max-size-buffers=10 ! \
+  videoconvert ! video/x-raw,format=RGB ! \
+  tensor_converter ! \
+  tensor_transform mode=arithmetic option=typecast:float32,add:0.0,div:255.0 ! \
+  queue leaky=2 max-size-buffers=10 ! \
+  tensor_filter latency=1 framework=tensorflow2-lite model=yolov8n_float16.tflite ! \
+  tensor_transform mode=transpose option=1:0:2:3 ! \
+  queue leaky=2 max-size-buffers=10 ! \
+  tensor_decoder mode=bounding_boxes option1=yolov8 option2=coco.txt option4=320:320 option5=320:320 ! \
+  videoconvert ! mix.sink_0 \
+  t. ! queue leaky=2 max-size-buffers=10 ! videoconvert ! mix.sink_1 \
+  compositor name=mix sink_0::zorder=2 sink_1::zorder=1 ! videoconvert ! autovideosink sync=false
