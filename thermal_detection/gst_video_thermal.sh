7d119f9b8372da97d68dfe642cbd0a4c4ded1f06
hairivlgy <mkkamarudin@videologyinc.com>
2026-02-19 16:24:05 +0800

    AB#2007: update thermal detection

--- thermal_detection/gst_video_thermal.sh
+++ thermal_detection/gst_video_thermal.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 gst-launch-1.0 \
   filesrc location=video_flir_1.mp4 ! \
   qtdemux ! h264parse ! avdec_h264 ! \
@@ -5,12 +7,14 @@ gst-launch-1.0 \
   t. ! queue leaky=no max-size-buffers=30 max-size-bytes=0 max-size-time=0 ! \
   videoconvert ! videoscale ! video/x-raw,format=RGB,width=320,height=320 ! \
   tensor_converter ! \
-  tensor_transform mode=arithmetic option=typecast:float32,add:0.0,div:255.0 ! \
   queue leaky=no max-size-buffers=30 max-size-bytes=0 max-size-time=0 ! \
-  tensor_filter latency=1 framework=tensorflow2-lite model=thermal_f16.tflite ! \
+  tensor_filter latency=1 framework=tensorflow2-lite model=thermal_yolov8n_320.tflite \
+  custom=Delegate:External,ExtDelegateLib:libvx_delegate.so ! \
   tensor_transform mode=transpose option=1:0:2:3 ! \
   queue leaky=no max-size-buffers=30 max-size-bytes=0 max-size-time=0 ! \
-  tensor_decoder mode=bounding_boxes option1=yolov8 option2=labels.txt option4=640:512 option5=320:320 ! \
+  tensor_transform mode=arithmetic option=typecast:float32,add:-17,mul:0.006334480829536915 ! \
+  tensor_decoder mode=bounding_boxes option1=yolov8 option2=thermal.txt option4=640:512 option5=320:320 ! \
+  queue leaky=no max-size-buffers=30 max-size-bytes=0 max-size-time=0 ! \
   videoscale ! videoconvert ! video/x-raw,width=640,height=512 ! \
   mix.sink_0 \
   t. ! queue leaky=no max-size-buffers=30 max-size-bytes=0 max-size-time=0 ! \
